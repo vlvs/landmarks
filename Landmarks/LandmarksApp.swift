6552d4b43a9205e9c2639f0d3ce24ee71324cdc0
Vander <vanderlei.alves@pipefy.com>
2023-04-13 19:59:12 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Vanderlei Alves on 13/04/23.
+//  
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
